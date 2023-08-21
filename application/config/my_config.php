668e23bb6756defc318c5623e852cd8c991dd822
unknown <amr_elngm6@yahoo.com>
2023-08-21 17:49:02 +0300

    .

--- application/config/my_config.php
+++ application/config/my_config.php
@@ -1,44 +1,61 @@
-<?php $config = array (
-  'default_page_url' => 'page/blank',
-  'product_name' => 'ChatPion',
-  'product_short_name' => 'ChatPion',
-  'product_version' => '8.5.4',
-  'slogan' => 'All-in-one Digital Marketing Platform',
-  'institute_address1' => 'Medians',
-  'institute_address2' => '140 - 26 July street, Zamalek, Beside Culture Wheel',
-  'institute_email' => 'admin@a.com',
-  'institute_mobile' => '01096869285',
-  'time_zone' => '',
-  'language' => '',
-  'facebook_poster_image_upload_limit' => '2',
-  'facebook_poster_video_upload_limit' => '15',
-  'autoreply_image_upload_limit' => '2',
-  'autoreply_video_upload_limit' => '15',
-  'comboposter_image_upload_limit' => '2',
-  'comboposter_video_upload_limit' => '15',
-  'vidcaster_image_upload_limit' => '2',
-  'vidcaster_video_upload_limit' => '200',
-  'messengerbot_image_upload_limit' => '2',
-  'messengerbot_video_upload_limit' => '15',
-  'messengerbot_audio_upload_limit' => '5',
-  'messengerbot_file_upload_limit' => '15',
-  'email_sending_option' => 'php_mail',
-  'force_https' => '0',
-  'enable_signup_form' => '1',
-  'enable_support' => '1',
-  'master_password' => '',
-  'messengerbot_subscriber_avatar_download_limit_per_cron_job' => '25',
-  'messengerbot_subscriber_profile_update_limit_per_cron_job' => '100',
-  'number_of_message_to_be_sent_in_try' => '10',
-  'update_report_after_time' => '5',
-  'broadcaster_number_of_message_to_be_sent_in_try' => '120',
-  'broadcaster_update_report_after_time' => '20',
-  'persistent_menu_copyright_text' => '',
-  'persistent_menu_copyright_url' => '',
-  'facebook_poster_group_enable_disable' => '0',
-  'facebook_poster_botenabled_pages' => '',
-  'sess_use_database' => false,
-  'sess_table_name' => 'ci_sessions',
-  'instagram_reply_enable_disable' => '1',
-  'central_webhook_verify_token' => '1257921330',
-);
+<?php 
+$config['default_page_url'] = 'page/blank';
+$config['product_name'] = 'MEDIANS';
+$config['product_short_name'] = 'MEDIANS';
+$config['product_version'] = '8.5.4';
+$config['slogan'] = 'All-in-one Digital Marketing Platform';
+
+$config['institute_address1'] = 'Medians';
+$config['institute_address2'] = '140 - 26 July street, Zamalek, Beside Culture Wheel';
+$config['institute_email'] = 'admin@a.com';
+$config['institute_mobile'] = '01096869285';
+
+$config['time_zone'] = '';
+$config['language'] = 'english';
+
+$config['facebook_poster_image_upload_limit'] = '2';
+$config['facebook_poster_video_upload_limit'] = '15';
+$config['autoreply_image_upload_limit'] = '2';
+$config['autoreply_video_upload_limit'] = '15';
+$config['comboposter_image_upload_limit'] = '2';
+$config['comboposter_video_upload_limit'] = '15';
+$config['vidcaster_image_upload_limit'] = '2';
+$config['vidcaster_video_upload_limit'] = '200';
+$config['messengerbot_image_upload_limit'] = '2';
+$config['messengerbot_video_upload_limit'] = '15';
+$config['messengerbot_audio_upload_limit'] = '5';
+$config['messengerbot_file_upload_limit'] = '15';
+
+$config['email_sending_option'] = 'php_mail';
+$config['enable_tracking_subscribers_last_interaction'] = 'yes';
+$config['is_rtl'] = '0';
+$config['force_https'] = '0';
+$config['enable_signup_form'] = '1';
+$config['instagram_reply_enable_disable'] = '1';
+$config['enable_signup_activation'] = '1';
+$config['enable_support'] = '0';
+
+$config['master_password'] = '';
+
+$config['messengerbot_subscriber_avatar_download_limit_per_cron_job'] = '25';
+$config['messengerbot_subscriber_profile_update_limit_per_cron_job'] = '100';
+
+$config['sms_api_access'] = '0';
+$config['email_api_access'] = '0';
+$config['number_of_sms_to_be_sent_in_try'] = '100';
+$config['update_sms_sending_report_after_time'] = '50';
+$config['number_of_email_to_be_sent_in_try'] = '100';
+$config['update_email_sending_report_after_time'] = '50';
+$config['enable_open_rate'] = '';
+$config['enable_click_rate'] = '';
+
+$config['persistent_menu_copyright_text'] = 'MEDIANS';
+$config['persistent_menu_copyright_url'] = 'https://bot.medianssolutions.com/';
+
+$config['facebook_poster_group_enable_disable'] = '';
+$config['facebook_poster_botenabled_pages'] = '0';
+$config['central_webhook_verify_token'] = '2056201257';
+$config['sess_use_database'] = FALSE;
+$config['sess_table_name'] = 'ci_sessions';
+$config['mail_service_id'] = '{"mailchimp":[],"sendinblue":[],"activecampaign":[],"mautic":[],"acelle":[]}';
+$config['delete_junk_data_after_how_many_days'] = '30';
